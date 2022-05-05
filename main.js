375d202ba2d984260c3ce331e6f0964213eda4fa
Johnny <johnny055238@gmail.com>
2022-05-06 00:31:51 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,116 @@
+let board_size = 7
+let round = true // false: white, true: black
+let interval_b
+let interval_w
+let timer_b = 1
+let timer_w = 1
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+function rander_board(size) {
+  let random_char = shuffle(charactors).slice(0, size * size)
+  round = true
+  clearInterval(interval_b)
+  clearInterval(interval_w)
+  timer_b = 1
+  timer_w = 1
+
+  $("#second_b").text("00")
+  $("#minute_b").text("00")
+  $("#second_w").text("00")
+  $("#minute_w").text("00")
+
+  $("#checkerboard").html("")
+  $("#checkerboard").get(0).style.setProperty("--board-size", size);
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      $("#checkerboard").append(`<div class="cell ${j}${i}">${random_char[i * size + j]}</div>`)
+    }
+  }
+}
+
+rander_board(7)
+
+function turn_change() {
+  round = !round
+  round ? $(".turn").html("黑") : $(".turn").html("白")
+  round ? startTimer_b() : startTimer_w()
+  round ? clearInterval(interval_w) : clearInterval(interval_b)
+}
+
+$("body").on("click", ".cell", function () {
+  $(".timer_div").css("pointer-events", "none")
+
+  let x = $(this).attr("class").split(" ")[1][0]
+  let y = $(this).attr("class").split(" ")[1][1]
+  $("#checkerboard").append(`<div class="piece"></div>`)
+  $(".piece").get(-1).style.setProperty("--x", x)
+  $(".piece").get(-1).style.setProperty("--y", y)
+
+  turn_change()
+
+  round ?
+    $(".piece").get(-1).style.setProperty("--bg-color", "#ffffffe6") :
+    $(".piece").get(-1).style.setProperty("--bg-color", "#000000e6")
+})
+
+function startTimer_b() {
+  let seconds, minutes
+
+  interval_b = setInterval(function () {
+    seconds = parseInt(timer_b % 60, 10)
+    minutes = parseInt(timer_b / 60, 10)
+    seconds = seconds < 10 && seconds >= 0 ? "0" + seconds : seconds
+    minutes = minutes < 10 && minutes >= 0 ? "0" + minutes : minutes
+
+    $("#second_b").text(seconds)
+    $("#minute_b").text(minutes)
+
+    timer_b++
+  }, 1000)
+}
+
+function startTimer_w() {
+  let seconds, minutes
+
+  interval_w = setInterval(function () {
+    seconds = parseInt(timer_w % 60, 10)
+    minutes = parseInt(timer_w / 60, 10)
+    seconds = seconds < 10 && seconds >= 0 ? "0" + seconds : seconds
+    minutes = minutes < 10 && minutes >= 0 ? "0" + minutes : minutes
+
+    $("#second_w").text(seconds)
+    $("#minute_w").text(minutes)
+
+    timer_w++
+  }, 1000)
+}
+
+$("select[name='board_size']").on("change", function () {
+  rander_board(this.value)
+})
+
+$(".turn_div").on("click", function () {
+  turn_change()
+})
+
+$(".regret").on("click", function () {
+  if ($(".piece").get(-1) === undefined) return false
+  $(".piece").get(-1).remove()
+  turn_change()
+})
+
+$(".timer_div").on("click", function () {
+  startTimer_b()
+  $(this).css("pointer-events", "none")
+})
+
+$(".setting_heading").on("click", function () {
+  $(".setting_container").slideToggle()
+})
